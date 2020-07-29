3f039548814774b80fd02012460f9752e8b4959f
KUD <kimud6003@gmail.com>
2020-07-29 19:19:42 +0900

    insert js

---
+++ script.js
@@ -0,0 +1,14 @@
+function add(num1,num2) {
+    console.log(num1+num2);
+}
+function sub(num1,num2) {
+    console.log(num1-num2);
+}
+
+function multi(num1,num2) {
+    console.log(num1*num2);
+}
+function asd(num1,num2) {
+    console.log(num1/num2);
+}
+
